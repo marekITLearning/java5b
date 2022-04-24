ae15a87b8723d860367364918e1f7808c2cb0b38
Marek Ladický <mla@digitalsystems.eu>
2022-04-24 02:53:29 +0200

    update

--- payara.bat
+++ payara.bat
@@ -1,5 +1,5 @@
 rmdir /s /q pmroot
-C:/devel/jdk-11.0.12/bin/java.exe ^
+C:/devel/jdk-11.0.13/bin/java.exe ^
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8088 ^
 -jar C:/devel/payara-micro-5.2021.2.jar ^
 --deploy backend_parent/webservice/target/webapp.war ^
